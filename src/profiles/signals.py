a350fc54446463ddc1710aafbe294a1e05a6a400
SillyIsaac <isaacjackson4659@gmail.com>
2024-04-06 19:42:17 -0400

    added main page view, posts view

--- src/profiles/signals.py
+++ src/profiles/signals.py
@@ -1,5 +1,5 @@
 from .models import Profile
-from django.contrivb.auth.models import User
+from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -9,4 +9,4 @@ def post_save_create_profile(sender, instance, created, *args, **kwargs):
     print(instance)
     print(created)
     if created: 
-        Profile.onjects.create(user=instance)
+        Profile.objects.create(user=instance)
