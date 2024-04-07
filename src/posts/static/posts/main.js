0be9b06be7c1e85941f77dbf869fcdf49739ef30
SillyIsaac <isaacjackson4659@gmail.com>
2024-04-06 20:23:47 -0400

    layout for ajax calls added

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -1,1 +1,18 @@
 console.log('hello world')
+
+const helloWorldBox = document.getElementById('hello-world')
+
+helloWorldBox.innerHTML = 'hello world'
+
+$.ajax({
+    type: 'GET',
+    url: '/hello-world',
+    success: function(response){
+        console.log('success', response)
+        helloWorldBox.textContent = response.text
+    },
+    error: function(error)
+    {
+        console.log('error', error)
+    }
+})
